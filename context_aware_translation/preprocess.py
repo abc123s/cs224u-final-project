73e5d50affb22a2b72f58661cc7ef4d0f63231ed
Peter Zhang <peter0zhang@gmail.com>
2021-06-02 01:47:14 -0700

    add other forms of data preprocessing necessary to conduct evaluation of how important context is for translation quality: - no context provided (with separator token before the line to be translated) - no context provided (without separator token before the line to be translated) - random context provided (randomly selected line from corpus)

--- context_aware_translation/preprocess.py
+++ context_aware_translation/preprocess.py
@@ -1,4 +1,5 @@
 import os
+import random
 import pandas as pd
 
 # helper function to construct path to corpus files
@@ -35,5 +36,60 @@ def preprocess(corpus, language_pair, split, context_type, break_token = '</s>')
                 prev_doc_id = doc_id
 
         return pd.DataFrame(data, columns=["prefix", "input_text", "target_text"])
-    else:   
-        raise NotImplementedError
+    if context_type == 'no-context-with-sep':
+        with open(corpus_path(corpus, language_pair, split, lang_1)) as lang_1_corpus, \
+             open(corpus_path(corpus, language_pair, split, lang_2)) as lang_2_corpus:
+            data = []
+            for raw_lang_1_line, raw_lang_2_line in zip(lang_1_corpus, lang_2_corpus):
+                lang_1_line = raw_lang_1_line.strip()
+                lang_2_line = raw_lang_2_line.strip()
+
+                lang_1_example = f' {break_token} {lang_1_line} {break_token}'
+                lang_2_example = f' {break_token} {lang_2_line} {break_token}'
+
+                data.append(["", lang_1_example, lang_2_example])
+        
+        return pd.DataFrame(data, columns=["prefix", "input_text", "target_text"])
+    
+    if context_type == 'no-context-without-sep':
+        with open(corpus_path(corpus, language_pair, split, lang_1)) as lang_1_corpus, \
+             open(corpus_path(corpus, language_pair, split, lang_2)) as lang_2_corpus:
+            data = []
+            for raw_lang_1_line, raw_lang_2_line in zip(lang_1_corpus, lang_2_corpus):
+                lang_1_line = raw_lang_1_line.strip()
+                lang_2_line = raw_lang_2_line.strip()
+
+                lang_1_example = f'{lang_1_line} {break_token}'
+                lang_2_example = f'{lang_2_line} {break_token}'
+
+                data.append(["", lang_1_example, lang_2_example])
+
+        return pd.DataFrame(data, columns=["prefix", "input_text", "target_text"])
+
+    if context_type == 'random-context':
+        with open(corpus_path(corpus, language_pair, split, lang_1)) as lang_1_corpus, \
+             open(corpus_path(corpus, language_pair, split, lang_2)) as lang_2_corpus:
+            lang_1_corpus_lines = [
+                raw_lang_1_line.strip() 
+                for raw_lang_1_line in lang_1_corpus.readlines()
+            ]
+            lang_2_corpus_lines = [
+                raw_lang_2_line.strip() 
+                for raw_lang_2_line in lang_2_corpus.readlines()
+            ]
+
+            data = []
+            for lang_1_line, lang_2_line in zip(lang_1_corpus_lines, lang_2_corpus_lines):
+                # select random context
+                random_index = random.randint(0, len(lang_1_corpus_lines) - 1)
+                random_lang_1_line = lang_1_corpus_lines[random_index]
+                random_lang_2_line = lang_2_corpus_lines[random_index]
+
+                lang_1_example = f'{random_lang_1_line} {break_token} {lang_1_line} {break_token}'
+                lang_2_example = f'{random_lang_2_line} {break_token} {lang_2_line} {break_token}'
+
+                data.append(["", lang_1_example, lang_2_example])
+        
+        return pd.DataFrame(data, columns=["prefix", "input_text", "target_text"])
+
+    raise NotImplementedError
