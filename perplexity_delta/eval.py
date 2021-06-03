362cee526a3ba7cd09a21074c46219f3a7159bce
Peter Zhang <peter0zhang@gmail.com>
2021-06-03 05:35:50 -0700

    script to calculate perplexity deltas for pre-trained language models for en, ja, es, fr on our evaluation corpus

---
+++ perplexity_delta/eval.py
@@ -0,0 +1,102 @@
+import os
+import subprocess
+import json
+from datetime import datetime
+
+import torch
+from transformers import GPT2LMHeadModel, AutoModelForCausalLM, GPT2TokenizerFast, T5TokenizerFast, AutoTokenizer
+from tqdm import tqdm
+
+from preprocess import preprocess
+
+language = "fr"
+
+language_models = {
+    "en": "gpt2-medium",
+    "ja": "rinna/japanese-gpt2-medium",
+    "es": "DeepESP/gpt2-spanish-medium",
+    "fr": 'antoiloui/belgpt2'
+}
+
+params = {
+    "LANGUAGE": language,
+    "MODEL": language_models[language],
+}
+
+# make experiment directory and save experiment params down
+date_string = datetime.now().strftime("%Y%m%d_%H%M")
+commit_string = subprocess.check_output(
+    ["git", "rev-parse", "--short", "HEAD"]).strip().decode("utf-8")
+experiment_dir = "experiments/" + date_string + "_" + commit_string
+os.mkdir(experiment_dir)
+os.mkdir(experiment_dir + "/logs")
+
+# save params down
+with open(experiment_dir + "/params.json", "w") as f:
+    json.dump(params, f, indent=4)
+
+# load pre-trained causal language model
+if params["LANGUAGE"] == "en":
+    tokenizer = GPT2TokenizerFast.from_pretrained(params["MODEL"])
+    model = GPT2LMHeadModel.from_pretrained(params["MODEL"])
+elif params["LANGUAGE"] == "ja":
+    tokenizer = T5TokenizerFast.from_pretrained(params["MODEL"])
+    tokenizer.do_lower_case = True  # due to some bug of tokenizer config loading
+    model = AutoModelForCausalLM.from_pretrained(params["MODEL"])
+else:
+    tokenizer = AutoTokenizer.from_pretrained(params["MODEL"])
+    model = AutoModelForCausalLM.from_pretrained(params["MODEL"])
+
+# compute perplexity delta on selected corpus
+examples = preprocess('four_way_parallel_corpus', params["LANGUAGE"], 'eval')[0:100]
+
+perplexities_with_context = []
+perplexities_without_context =[]
+perplexity_deltas = []
+percent_perplexity_deltas = []
+for i in tqdm(range(len(examples))):
+    context_sentence, target_sentence = examples[i]
+    context_encoding = tokenizer(context_sentence).input_ids
+    target_encoding = tokenizer(target_sentence).input_ids
+
+    # compute perplexity with context
+    input_ids_with_context = torch.tensor([[*context_encoding, *target_encoding]])
+    target_ids_with_context = input_ids_with_context.clone()
+    # ignore loss from context sentence tokens
+    target_ids_with_context[:,:-len(target_encoding)] = -100
+
+    with torch.no_grad():
+        loss_with_context = model(input_ids_with_context, labels=target_ids_with_context)[0]
+        # perplexity is the exponentiation of the cross-entropy loss
+        perplexity_with_context = torch.exp(loss_with_context).item()
+    
+    perplexities_with_context.append(perplexity_with_context)
+
+    # compute perplexity without context
+    input_ids_without_context = torch.tensor([[*target_encoding]])
+    target_ids_without_context = input_ids_without_context.clone()
+
+    with torch.no_grad():
+        loss_without_context = model(input_ids_without_context, labels=target_ids_without_context)[0]
+        # perplexity is the exponentiation of the cross-entropy loss
+        perplexity_without_context = torch.exp(loss_without_context).item()
+
+    perplexities_without_context.append(perplexity_without_context)
+
+    perplexity_deltas.append(perplexity_without_context - perplexity_with_context)
+    percent_perplexity_deltas.append((perplexity_without_context - perplexity_with_context) / perplexity_without_context)
+
+average_perplexity_with_context = sum(perplexities_with_context) / len(perplexities_with_context)
+average_perplexity_without_context = sum(perplexities_without_context) / len(perplexities_without_context)
+average_perplexity_deltas = sum(perplexity_deltas) / len(perplexity_deltas)
+average_percent_perplexity_deltas = sum(percent_perplexity_deltas) / len(percent_perplexity_deltas)
+
+# save results
+# save params down
+with open(experiment_dir + "/results.json", "w") as f:
+    json.dump({
+        "AVERAGE_PERPLEXITY_WITH_CONTEXT": average_perplexity_with_context,
+        "AVERAGE_PERPLEXITY_WITHOUT_CONTEXT": average_perplexity_without_context,
+        "AVERAGE_PERPLEXITY_DELTA": average_perplexity_deltas,
+        "AVERAGE_PERCENT_PERPLEXITY_DELTA": average_percent_perplexity_deltas,
+    }, f, indent=4)
